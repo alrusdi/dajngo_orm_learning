31d24197d2e63510d8b79f637b14e41af5e70db8
alrusdi <alrusdi@gmail.com>
2021-12-19 12:52:05 +0500

    Fix verbose names

--- shooting1/models.py
+++ shooting1/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(title="Название города", max_length=255, unique=True)
+    name = models.CharField(verbose_name="Название города", max_length=255, unique=True)
 
     def __repr__(self):
         return f"City: {self.name}"
 
 
 class Shooter(models.Model):
-    name = models.CharField(title="Фамилия и имя игрока", max_length=255, unique=True)
+    name = models.CharField(verbose_name="Фамилия и имя игрока", max_length=255, unique=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
 
     def __repr__(self):
@@ -18,7 +18,7 @@ class Shooter(models.Model):
 
 class Result(models.Model):
     shooter = models.ForeignKey(Shooter, on_delete=models.CASCADE)
-    score = models.PositiveIntegerField()  # Количество очков набранных данным стрелком
+    score = models.PositiveIntegerField(verbose_name="Количество очков")
 
     def __repr__(self):
         return f"Result: {self.shooter.name} {self.score}"
