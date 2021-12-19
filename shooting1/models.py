0de1d2085d1138a1b851b3097fe766c86a690dd3
alrusdi <alrusdi@gmail.com>
2021-12-19 12:54:27 +0500

    Replace __repr__ by __str__

--- shooting1/models.py
+++ shooting1/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(verbose_name="Название города", max_length=255, unique=True)
 
-    def __repr__(self):
+    def __str__(self):
         return f"City: {self.name}"
 
 
@@ -12,7 +12,7 @@ class Shooter(models.Model):
     name = models.CharField(verbose_name="Фамилия и имя игрока", max_length=255, unique=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
 
-    def __repr__(self):
+    def __str__(self):
         return f"Shooter: {self.name}"
 
 
@@ -20,5 +20,5 @@ class Result(models.Model):
     shooter = models.ForeignKey(Shooter, on_delete=models.CASCADE)
     score = models.PositiveIntegerField(verbose_name="Количество очков")
 
-    def __repr__(self):
+    def __str__(self):
         return f"Result: {self.shooter.name} {self.score}"
